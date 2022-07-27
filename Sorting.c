e1817c48bcd32677361e8382ea5e37227a437a8b
khushisingh4218 <khushisingh4218@gmail.com>
2022-07-27 23:07:52 +0530

    Quick sort

--- Sorting.c
+++ Sorting.c
@@ -118,3 +118,52 @@ void bubbleSort(int arr[], int n)
         }
     }
 }
+
+// Quick sort
+void quickSort(int arr[], int first, int last)
+{
+
+    int i, j, pivot, temp;
+
+    if (first < last)
+    {
+
+        pivot = first;
+
+        i = first;
+
+        j = last;
+
+        while (i < j)
+        {
+
+            while (arr[i] <= arr[pivot] && i < last)
+
+                i++;
+
+            while (arr[j] > arr[pivot])
+
+                j--;
+
+            if (i < j)
+            {
+
+                temp = arr[i];
+
+                arr[i] = arr[j];
+
+                arr[j] = temp;
+            }
+        }
+
+        temp = arr[pivot];
+
+        arr[pivot] = arr[j];
+
+        arr[j] = temp;
+
+        quickSort(arr, first, j - 1);
+
+        quickSort(arr, j + 1, last);
+    }
+}
