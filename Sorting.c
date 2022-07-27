65a24c351467d1cd1b7076c7f9af8875c8fb8013
khushisingh4218 <khushisingh4218@gmail.com>
2022-07-27 23:04:04 +0530

    Selection sort

--- Sorting.c
+++ Sorting.c
@@ -71,3 +71,26 @@ void mergeSort(int arr[], int l, int r)
         merge(arr, l, m, r);
     }
 }
+
+//selection sort
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
