48966f528ba6a72fbdb4d318e61bbc9e1aee6fa1
khushisingh4218 <khushisingh4218@gmail.com>
2022-07-27 23:05:03 +0530

    Bubble sort

--- Sorting.c
+++ Sorting.c
@@ -94,3 +94,27 @@ void selectionSort(int arr[], int n)
         }
     }
 }
+
+// Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        /*Swapping adjacent elements of array such that
+        largest element reaches to arr[n-1]*/
+
+        int flag = 0;
+        for (int j = 0; j <= n - 1; j++)
+        {
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(&arr[j], &arr[j + 1]);
+                    flag = 1;
+                }
+            }
+            if (flag = 0)
+                break;
+        }
+    }
+}
