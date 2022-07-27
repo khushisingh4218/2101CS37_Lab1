a23a976aaa73ef9dab17da4bf3d5372ac461ac27
khushisingh4218 <khushisingh4218@gmail.com>
2022-07-27 22:44:05 +0530

    int main()

--- Calculator.c
+++ Calculator.c
@@ -1,5 +1,49 @@
 #include<stdio.h>
-int add(int m,int n);
-int subtract(int m,int n);
-int multiply(int m,int n);
-int divide(int m,int n);
+void add(float m,float n);
+void subtract(float m,float n);
+void multiply(float m,float n);
+void divide(float m,float n);
+int main()
+{
+    printf("1. Addition\n2.Subtraction\n3.Multiplication\n4.Division\n");
+    printf("Enter the operation(1/2/3/4):\n");
+    int choice;
+    scanf("%d", &choice);
+    // taking input of choice of operation
+    float a, b;
+    printf("Enter first number a:\n");
+    scanf("%f", &a);
+    printf("Enter second number b:\n");
+    scanf("%f", &b);
+    // taking input of numbers
+    switch (choice)
+    {
+    case 1:
+        add(a, b);
+        break;
+
+    case 2:
+        subtract(a, b);
+        break;
+
+    case 3:
+        multiply(a, b);
+        break;
+
+    case 4:
+        if (b == 0)
+        {
+            printf("Division by zero is not possible!");
+        }
+        else
+        {
+            divide(a, b);
+        }
+        break;
+
+    default:
+        printf("Invalid operation!\n");
+        break;
+    }
+    return 0;
+}
